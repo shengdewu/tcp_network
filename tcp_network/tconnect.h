e766a6489590608fe9e4a97bb7f9212317268b0a
boofish <7862221042qq.com>
2019-03-09 10:28:35 +0800

    update tconect

--- tcp_network/tconnect.h
+++ tcp_network/tconnect.h
@@ -1,15 +1,33 @@
 #pragma once 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 class tconnect
 {
 public:
-    tconnect(int fd);
+    tconnect(int fd)_socket_fd(fd);
     ~tconnect();
 
     virtual bool send_msg(char *data, unsigned int length);
+
+    inline void close();
 
 protected:
     virtual  void handle_network_data(char *data, unsigned int length) = 0;
     virtual  void handle_create() = 0;
     virtual  void handle_close() = 0;
+
+private:
+    int _socket_fd;
 };
+
+//
+//inline
+//
+void tconnect::close()
+{
+    ::close(_socket_fd);
+}
